4dd90da61c7091757662df0905b3ca38f31cd8e7
Adam McCrea <adam@adamlogic.com>
2022-03-14 12:36:50 -0400

    API request exception handling

--- src/judoscale/core/adapter_api_client.py
+++ src/judoscale/core/adapter_api_client.py
@@ -6,16 +6,13 @@ import json
 logger = logging.getLogger(__name__)
 
 class AdapterApiClient:
-    def __init__(self):
-        pass
-
     def post_metrics(self, metrics):
         try:
             logger.debug("[Judoscale] Posting metrics to Judoscale adapter API")
             payload = json.dumps(metrics)
             r = requests.post(config.api_base_url + '/adapter/v1/reports', timeout=5, json=payload)
-        except:
-            logger.debug("TODO: handle API exception")
+        except requests.RequestException as e:
+            logger.warn("[Judoscale] Adapter API request failed - {}".format(e))
 
 
 api_client = AdapterApiClient()
