57c0358188b15ca371e5c1d3c853be2c035b1c66
Adam McCrea <adam@adamlogic.com>
2022-03-23 13:07:42 -0400

    Fix timezone-dependent test

--- test/judoscale/reporter_test.py
+++ test/judoscale/reporter_test.py
@@ -5,11 +5,11 @@ from datetime import datetime
 class TestReporter:
     def test_build_report(self):
         reporter = Reporter()
-        dt = datetime.fromisoformat("2012-12-12T12:12:00")
+        dt = datetime.fromisoformat("2012-12-12T12:12:00+00:00")
         metric = Metric(measurement="test", datetime=dt, value=123)
 
         report = reporter._build_report([metric])
 
         assert list(report.keys()).sort() == ['config', 'dyno', 'pid', 'metrics'].sort()
         assert len(report['metrics']) == 1
-        assert report['metrics'][0] == [1355332320, 123, "test", None]
+        assert report['metrics'][0] == [1355314320, 123, "test", None]
