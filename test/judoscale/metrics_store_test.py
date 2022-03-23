359b6d5385ba7af78eee48c929a10fdd071810b8
Adam McCrea <adam@adamlogic.com>
2022-03-23 13:29:34 -0400

    Avoid memory leak by ensuring metrics are being flushed

--- test/judoscale/metrics_store_test.py
+++ test/judoscale/metrics_store_test.py
@@ -1,4 +1,5 @@
 from judoscale.core.metrics_store import MetricsStore
+import time
 
 class TestMetricsStore:
     def test_add_and_flush(self):
@@ -8,3 +9,17 @@ class TestMetricsStore:
 
         assert store.flush() == [22, 11]
         assert store.flush() == []
+
+    def test_max_flush_interval(self):
+        store = MetricsStore(max_flush_interval=0.0001)
+        store.add(11)
+        time.sleep(0.01)
+
+        # This add should be a no-op since it's been too long since the last flush
+        store.add(22)
+
+        assert store.flush() == [11]
+
+        store.add(22)
+
+        assert store.flush() == [22]
