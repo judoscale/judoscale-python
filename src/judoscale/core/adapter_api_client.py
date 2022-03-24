38579103f8adc59c4e4d8d26c4e491ecc4f50679
Adam McCrea <adam@adamlogic.com>
2022-03-24 16:08:33 -0400

    Fix JSON payload
    The requests package handles JSON serialization, so this was serializing twice.

--- src/judoscale/core/adapter_api_client.py
+++ src/judoscale/core/adapter_api_client.py
@@ -10,8 +10,7 @@ class AdapterApiClient:
     def post_report(self, report):
         try:
             logger.debug("Posting metrics to Judoscale adapter API")
-            payload = json.dumps(report)
-            requests.post(config.api_base_url + "/v1/metrics", timeout=5, json=payload)
+            requests.post(config.api_base_url + "/v1/metrics", timeout=5, json=report)
         except requests.RequestException as e:
             logger.warn("Adapter API request failed - {}".format(e))
 
