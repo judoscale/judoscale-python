02168221d3edc67722f0b7a1f9a5a61f94841c65
Mara Karagianni <mara@multiplace.org>
2022-07-21 15:32:04 +0300

    Debug workers and dynos PID

--- src/judoscale/core/adapter_api_client.py
+++ src/judoscale/core/adapter_api_client.py
@@ -9,8 +9,10 @@ logger = logging.getLogger(__name__)
 class AdapterApiClient:
     def post_report(self, report):
         try:
-            logger.debug("Posting metrics to Judoscale adapter API")
-            requests.post(config.api_base_url + "/v1/metrics", timeout=5, json=report)
+            url_metrics = config.api_base_url + "/v1/metrics"
+            logger.debug(
+                f"Posting metrics {report} to Judoscale adapter API {url_metrics}")
+            requests.post(url_metrics, timeout=5, json=report)
         except requests.RequestException as e:
             logger.warning("Adapter API request failed - {}".format(e))
 
