92d2999953f64fca8af376aacc0a9e0b135570cb
Mara Karagianni <mara@multiplace.org>
2022-09-29 14:55:41 +0200

    Move common middleware django and flask functions to core

--- judoscale/django/middleware.py
+++ judoscale/django/middleware.py
@@ -1,34 +1,21 @@
-import re
-import logging
-from datetime import datetime
 from judoscale.core.metrics_store import metrics_store
-from judoscale.core.metric import Metric
+from judoscale.core.metric import RequestMetrics
 from judoscale.core.reporter import reporter
-
-logger = logging.getLogger(__name__)
 
 
 class RequestQueueTimeMiddleware:
+    """ Django middleware for query metrics report"""
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         request_start_header = request.META.get("HTTP_X_REQUEST_START", "")
 
-        # Remove non-digits. This removes the "t=" prefix added by some web servers (NGINX).
-        # NGINX also reports this time as fractional seconds with millisecond resolution,
-        # so removing the decimal gives us integer milliseconds (same as Heroku).
-        request_start_header = re.sub(r"\D", "", request_start_header)
-
-        if len(request_start_header) > 0:
-            now = datetime.now()
-            request_start_timestamp_ms = int(request_start_header)
-            current_timestamp_ms = now.timestamp() * 1000
-            queue_time_ms = current_timestamp_ms - request_start_timestamp_ms
-            metric = Metric(measurement="queue_time", datetime=now, value=queue_time_ms)
-
+        request_metric = RequestMetrics(request_start_header)
+        metric = request_metric.get_queue_time_metric_from_header()
+        if metric:
             metrics_store.add(metric)
-            logger.debug("queue_time={}ms".format(round(queue_time_ms, 2)))
-            reporter.ensure_running()
+        reporter.ensure_running()
 
         return self.get_response(request)
--- judoscale/flask/middleware.py
+++ judoscale/flask/middleware.py
@@ -1,14 +1,8 @@
-import re
-import logging
-from datetime import datetime
-
 from werkzeug.wrappers import Request
 
 from judoscale.core.metrics_store import metrics_store
-from judoscale.core.metric import Metric
+from judoscale.core.metric import RequestMetrics
 from judoscale.core.reporter import reporter
-
-logger = logging.getLogger(__name__)
 
 
 class RequestQueueTimeMiddleware:
@@ -18,35 +12,13 @@ class RequestQueueTimeMiddleware:
         self.app = app
 
     def __call__(self, environ, start_response):
-        """
-        Remove non-digits:
-            This removes the "t=" prefix added by some web servers (NGINX).
-        Remove decimal:
-            NGINX also reports this time as fractional seconds with millisecond
-            resolution, so removing the decimal gives us integer milliseconds
-            (same as Heroku).
-        Then, we calculate the timelapse between request start and finish,
-            log and send it to judoscale reporter.
-        """
-
         request = Request(environ)
-        logger.debug(request)
-
         request_start_header = request.environ.get("HTTP_X_REQUEST_START", "")
-        request_start_header = re.sub(r"\D", "", request_start_header)
 
-        if len(request_start_header) > 0:
-            logger.debug(f"START X {request_start_header}")
-            now = datetime.now()
-            request_start_timestamp_ms = int(request_start_header)
-            current_timestamp_ms = now.timestamp() * 1000
-            queue_time_ms = current_timestamp_ms - request_start_timestamp_ms
-            metric = Metric(measurement="queue_time",
-                            datetime=now,
-                            value=queue_time_ms)
-
+        request_metric = RequestMetrics(request_start_header)
+        metric = request_metric.get_queue_time_metric_from_header()
+        if metric:
             metrics_store.add(metric)
-            logger.debug("queue_time={}ms".format(round(queue_time_ms, 2)))
-            reporter.ensure_running()
+        reporter.ensure_running()
 
         return self.app(environ, start_response)
