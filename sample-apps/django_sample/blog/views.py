8454d9fc03d98c12fdce1b907ce55738c8283a6a
Karl Sutt <karl@sutt.ee>
2023-01-31 14:33:39 +0200

    Update Django sample app settings

--- sample-apps/django_sample/blog/views.py
+++ sample-apps/django_sample/blog/views.py
@@ -9,7 +9,8 @@ logger = logging.getLogger(__name__)
 def index(request):
     # Log message in level warning as this is Django's default logging level
     logger.warning("Hello, world")
+    catcher_url = settings.JUDOSCALE["API_BASE_URL"].replace("/inspect/", "/p/")
     return HttpResponse(
         "Judoscale Django Sample App. "
-        f"<a target='_blank' href={settings.JUDOSCALE['API_BASE_URL']}>Metrics</a>"
+        f"<a target='_blank' href={catcher_url}>Metrics</a>"
     )
