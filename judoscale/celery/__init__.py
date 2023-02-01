103e42b02e822dbf9de6eddce578d236211a4b3f
Karl Sutt <karl@sutt.ee>
2023-02-01 16:43:15 +0200

    Refactor before_task_publish

--- judoscale/celery/__init__.py
+++ judoscale/celery/__init__.py
@@ -7,26 +7,17 @@ from judoscale.celery.collector import CeleryMetricsCollector
 from judoscale.core.config import config as judoconfig
 from judoscale.core.reporter import reporter
 
-before_publish_handler = None
 
-
-def before_publish(collector: CeleryMetricsCollector):
-    def _before_publish(*args, routing_key, properties={}, **kwargs):
-        collector.queues.add(routing_key)
-        properties["published_at"] = time.time()
-
-    return _before_publish
+@before_task_publish.connect
+def before_publish(*args, properties={}, **kwargs):
+    properties["published_at"] = time.time()
 
 
 def judoscale_celery(celery: Celery, extra_config: dict = {}) -> None:
-    global before_publish_handler
-
     celery.conf.task_send_sent_event = True
 
     judoconfig.merge(extra_config)
     collector = CeleryMetricsCollector(config=judoconfig, broker=celery)
-
-    before_publish_handler = before_task_publish.connect(before_publish(collector))
 
     reporter.add_collector(collector)
     reporter.ensure_running()
