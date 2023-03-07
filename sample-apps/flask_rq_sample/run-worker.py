2d3c365dbf9c2413adbd1433bd27a8c9f84760e3
Karl Sutt <karl@sutt.ee>
2023-03-07 14:38:11 +0200

    Simplify sample worker script for RQ

--- sample-apps/flask_rq_sample/run-worker.py
+++ sample-apps/flask_rq_sample/run-worker.py
@@ -1,15 +1,21 @@
 import sys
 
-from app.settings import BaseConfig
-from redis import Redis
+from app.app import create_app
+from rq.worker import Worker
 
-from judoscale.rq.worker import Worker
-
-connection = Redis()
+app = create_app()
 queues = sys.argv[1:]
 
+# Instantiate a worker with the queues you want to consume from
+# and the connection to Redis.
+# In this example, we're using the Redis connection that
+# we established in the app factory.
+#
+worker = Worker(queues, connection=app.redis)
 
-if __name__ == "__main__":
-    worker = Worker(queues, connection=connection)
-    worker.start_reporter(extra_config=BaseConfig.JUDOSCALE)
-    worker.work(with_scheduler=True)
+# Run the worker *in the app context* which will
+# 1. Ensure that the worker has access to the app context; and
+# 2. Set up the Judoscale adapter.
+#
+with app.app_context():
+    worker.work()
