977fe67c8a2fcfeb312077dfcf35a99d9ad90a7a
Karl Sutt <karl@sutt.ee>
2023-03-07 14:43:23 +0200

    Add judoscale.rq Django app and a sample app

---
+++ judoscale/rq/apps.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+from django.apps import AppConfig
+from django.conf import settings
+from redis import Redis
+
+from judoscale.core.config import config as judoconfig
+from judoscale.core.reporter import reporter
+from judoscale.rq import judoscale_rq
+
+logger = logging.getLogger(__name__)
+
+
+class JudoscaleRQConfig(AppConfig):
+    name = "judoscale.rq"
+    label = "judoscale_rq"
+    verbose_name = "Judoscale (RQ)"
+
+    def ready(self):
+        judoconfig.merge(getattr(settings, "JUDOSCALE", {}))
+
+        if judoconfig.api_base_url is None:
+            logger.info("Not activated - No API URL provided")
+            return
+
+        judoscale_rq(self.redis_connection)
+        reporter.ensure_running()
+
+    @property
+    def redis_connection(self):
+        """
+        Return a Redis connection from an RQ queue configuration
+        """
+        if redis_config := settings.JUDOSCALE.get("REDIS"):
+            config = {k.lower(): v for k, v in redis_config.items()}
+            if redis_url := config.get("url"):
+                return Redis.from_url(redis_url)
+            else:
+                return Redis(**config)
+        elif redis_url := os.getenv("REDIS_URL"):
+            return Redis.from_url(redis_url)
+        else:
+            raise RuntimeError(
+                "Missing Redis connection configuration. Please set either "
+                "settings.JUDOSCALE['REDIS'] or REDIS_URL environment variable."
+            )
---
+++ sample-apps/django_rq_sample/django_rq_sample/urls.py
@@ -0,0 +1,26 @@
+"""django_sample URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from blog.views import index, many_tasks, one_task
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("task", one_task, name="one_task"),
+    path("batch_task", many_tasks, name="many_tasks"),
+    path("admin/", admin.site.urls),
+    path("django-rq/", include("django_rq.urls")),
+]
