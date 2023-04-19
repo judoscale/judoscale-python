398475fdad15477bdf886d326f9612f111a41005
Karl Sutt <karl@sutt.ee>
2023-04-19 14:22:51 +0300

    Make RequestQueueTimeMiddleware generic
    Provide middleware clases for specific frameworks

--- judoscale/asgi/middleware.py
+++ judoscale/asgi/middleware.py
@@ -1,8 +1,6 @@
 from importlib import metadata
 from typing import Mapping
 
-from starlette.requests import Request
-
 from judoscale.core.adapter import Adapter, AdapterInfo
 from judoscale.core.config import config as judoconfig
 from judoscale.core.metric import Metric
@@ -11,15 +9,15 @@ from judoscale.core.reporter import reporter
 
 
 class RequestQueueTimeMiddleware:
-    """FastAPI middleware for query metrics report""" ""
+    """Generic middleware class for reporting request queue time metrics to Judoscale"""
 
     def __init__(self, app, extra_config: Mapping = {}, **kwargs):
         self.app = app
         judoconfig.update(extra_config)
         self.collector = WebMetricsCollector(judoconfig)
         adapter = Adapter(
-            identifier="judoscale-fastapi",
-            adapter_info=AdapterInfo(platform_version=metadata.version("fastapi")),
+            identifier=f"judoscale-{self.platform}",
+            adapter_info=AdapterInfo(platform_version=metadata.version(self.platform)),
             metrics_collector=self.collector,
         )
         reporter.add_adapter(adapter)
@@ -30,10 +28,31 @@ class RequestQueueTimeMiddleware:
             await self.app(scope, receive, send)
             return
 
-        request = Request(scope)
-        request_start = request.headers.get("x-request-start", "")
-        if metric := Metric.for_web(request_start):
-            self.collector.add(metric)
+        for header, value in scope["headers"]:
+            if header.lower() == b"x-request-start":
+                request_start = value.decode()
+                if metric := Metric.for_web(request_start):
+                    self.collector.add(metric)
+                break
+
         reporter.ensure_running()
 
         await self.app(scope, receive, send)
+
+
+class StarletteRequestQueueTimeMiddleware(RequestQueueTimeMiddleware):
+    """Starlette middleware for reporting request queue time metrics to Judoscale"""
+
+    platform = "starlette"
+
+
+class FastAPIRequestQueueTimeMiddleware(RequestQueueTimeMiddleware):
+    """FastAPI middleware for reporting request queue time metrics to Judoscale"""
+
+    platform = "fastapi"
+
+
+class QuartRequestQueueTimeMiddleware(RequestQueueTimeMiddleware):
+    """Quart middleware for reporting request queue time metrics to Judoscale"""
+
+    platform = "quart"
