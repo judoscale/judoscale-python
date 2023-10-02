badb30ee36095fa4c66ada0ba24b424cef4c74a2
Karl Sutt <karl@sutt.ee>
2023-10-02 17:53:28 +0300

    Add link to Django 3.2 changes

--- judoscale/django/__init__.py
+++ judoscale/django/__init__.py
@@ -2,4 +2,5 @@ import django
 
 if django.VERSION < (3, 2, 0):
     # Only define default_app_config when using a version earlier than 3.2
+    # https://docs.djangoproject.com/en/4.2/releases/3.2/#what-s-new-in-django-3-2
     default_app_config = "judoscale.django.apps.JudoscaleDjangoConfig"
