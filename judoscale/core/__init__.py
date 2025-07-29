77031e0a4ee9c933378832f9c6dad030d7999f19
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2025-07-29 14:19:46 -0300

    Move shutdown signal handler to judoscale core init
    While this is related to the reporter and the fact it is a thread we
    need to gracefully shutdown, it's not specific to the reporter and more
    to the core package itself.
    
    Plus, we'll probably need to extend this to shutdown the utilization
    tracker thread soon, so it makes sense to put it in a more "global"
    place.

---
+++ judoscale/core/__init__.py
@@ -0,0 +1,21 @@
+import signal
+import sys
+
+from judoscale.core.reporter import reporter
+
+previous_handler = signal.getsignal(signal.SIGTERM)
+
+def graceful_shutdown(signum, frame):
+    reporter.signal_handler(signum, frame)
+    # If there was a previous handler and it's not SIG_DFL or SIG_IGN, call it
+    if callable(previous_handler) and previous_handler not in (
+        signal.SIG_DFL,
+        signal.SIG_IGN,
+    ):
+        previous_handler(signum, frame)
+    else:
+        # If no previous handler, use default behavior (exit the process)
+        sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, graceful_shutdown)
